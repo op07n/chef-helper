3746bc3bb35f0f692dc3a14533d2cdb889e00f07
op07n <39182105+op07n@users.noreply.github.com>
2020-05-01 19:42:49 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,76 @@
-import org.gradle.api.JavaVersion.VERSION_11
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-	kotlin("jvm") version "1.3.50"
-	id("application")
-	id("org.openjfx.javafxplugin") version "0.0.8"
-	id("com.github.johnrengelman.shadow") version "5.1.0"
+  kotlin("jvm") version "1.3.71"
+  id("java")
+  id("com.github.johnrengelman.shadow") version "5.2.0"
+  id("application")
+  id("org.openjfx.javafxplugin") version "0.0.8"
 }
-
-val applicationVersion: String by project
 
 group = "com.imperfect.tools.chef.helper"
-version = applicationVersion
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-java {
-	sourceCompatibility = VERSION_11
-	targetCompatibility = VERSION_11
-}
+val javafxVersion = "11.0.2"
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	
-	implementation("javax.inject:javax.inject:1")
-	implementation("com.google.inject:guice:4.2.2:no_aop")
-	
-	implementation(platform("com.fasterxml.jackson:jackson-bom:2.10.0"))
-	implementation("com.fasterxml.jackson.core:jackson-databind")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	
-	implementation("org.openjfx:javafx-controls:13")
-	implementation("no.tornado:tornadofx:1.7.19")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
+  implementation("org.slf4j:slf4j-log4j12:1.7.30")
+  implementation("com.google.guava:guava:29.0-jre")
+  implementation("com.atilika.kuromoji:kuromoji-ipadic:0.9.0")
+  implementation("org.openjfx:javafx-controls:14")
+  implementation("no.tornado:tornadofx:1.7.17")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion.version:win")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion:linux")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion:mac")
+
+  testImplementation("org.mockito:mockito-junit-jupiter:3.3.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
-javafx {
-	// Declare the javafx modules we need to use
-	modules("javafx.controls")
-}
-
-tasks.jar {
-	manifest {
-		attributes["Main-Class"] = "${project.group}.MainKt"
-	}
-}
-
-application {
-    mainClassName = "${project.group}.MainKt"
+tasks.withType<Test> {
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions {
+    freeCompilerArgs = listOf(
+      "-Xjsr305=strict",
+      "-Xopt-in=kotlin.ExperimentalUnsignedTypes"
+    )
+    jvmTarget = "11"
+  }
+}
+
+tasks {
+  named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("kotlin_tornadofx")
+    mergeServiceFiles()
+    manifest {
+      attributes(mapOf("Main-Class" to "com.github.csolem.gradle.shadow.kotlin.example.App"))
+    }
+  }
+}
+
+tasks {
+  build {
+    dependsOn(shadowJar)
+  }
+}
+
+javafx {
+  version = javafxVersion
+  modules = listOf("javafx.controls", "javafx.graphics", "javafx.fxml")
+}
+
+application {
+  mainClassName = "com.imperfect.tools.chef.helper.MainKt"
 }
